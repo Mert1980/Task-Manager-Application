f5ea40cdb1ec41177df65c09486510c742382661
Mert Demirok <mertdemirok80@gmail.com>
2020-01-01 22:45:57 +0100

    inserting multiple documents to database with insertMany method

--- mongodb.js
+++ mongodb.js
@@ -13,18 +13,35 @@ MongoClient.connect(
     if (error) {
       return console.log("Unable to connect to the database!");
     }
-    console.log("successfull");
     const db = client.db(databaseName);
-    db.collection("users").insertOne(
-      {
-        name: "Mert",
-        age: 39
-      },
+    // db.collection("users").insertOne( // --> users is the name of the collection
+    //   {
+    //     name: "Mert",
+    //     age: 39
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user!");
+    //     }
+    //     console.log(result.ops); // --> array of documents
+    //   }
+    // );
+    db.collection("users").insertMany(
+      [
+        {
+          name: "Yigit",
+          age: 30
+        },
+        {
+          name: "Kaan",
+          age: 35
+        }
+      ],
       (error, result) => {
         if (error) {
-          return console.log("Unable to insert user!");
+          return console.log("Unable to insert users!");
         }
-        console.log(result.ops); // --> array of documents
+        console.log(result.ops);
       }
     );
   }
