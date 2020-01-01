4d0665f2de47faf2ffc2cec45354102d1ee03e85
Mert Demirok <mertdemirok80@gmail.com>
2020-01-01 22:07:06 +0100

    callback function(error, result) added to the  insertOne method

--- mongodb.js
+++ mongodb.js
@@ -6,14 +6,26 @@ const MongoClient = mongodb.MongoClient; // --> connects to the database
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
-MongoClient.connect(connectionURL, {useUnifiedTopology: true}, (error, client)=>{
-    if (error){
-        return console.log('Unable to connect to the database!')
+MongoClient.connect(
+  connectionURL,
+  { useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to the database!");
     }
-    console.log('successfull')
-    const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name:'Mert',
+    console.log("successfull");
+    const db = client.db(databaseName);
+    db.collection("users").insertOne(
+      {
+        name: "Mert",
         age: 39
-    })
-})
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert user!");
+        }
+        console.log(result.ops); // --> array of documents
+      }
+    );
+  }
+);
