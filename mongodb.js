08a3c1c5af68be28843e2c8464b5a35ecfe38000
Mert Demirok <mertdemirok80@gmail.com>
2020-01-01 01:05:50 +0100

    initial files created. Connection set up from node.js to mongo database

---
+++ mongodb.js
@@ -0,0 +1,14 @@
+// CRUD create read update delete
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient; // --> connects to the database
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(connectionURL, {useUnifiedTopology: true}, (error, client)=>{
+    if (error){
+        return console.log('Unable to connect to the database!')
+    }
+    console.log('connected correctly!')
+})
