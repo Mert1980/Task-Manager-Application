82788fa8708664f7d419658d9f127f37a3bdffcd
Mert Demirok <mertdemirok80@gmail.com>
2020-01-03 09:06:21 +0100

    callback example-2 added

--- playground/1-callbacks.js
+++ playground/1-callbacks.js
@@ -1,6 +1,7 @@
 const doWorkCallback = callback => {
   setTimeout(() => {
-    callback("This is my error", undefined);
+    // callback("This is my error", undefined);
+    callback(undefined, [1,2,3])
   }, 2000);
 };
 
