a1bbba6bea5fe893d0ae74b84a6e40be8c3ccc7e
Mert Demirok <mertdemirok80@gmail.com>
2020-01-03 23:40:30 +0100

    authentication removed from the connection URL

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  `mongodb://Mert:${process.env.MONGODB_PSWD}@127.0.0.1:27017/task-manager-api`,
+  `mongodb://127.0.0.1:27017/task-manager-api`,
   {
     useUnifiedTopology: true,
     useCreateIndex: true // --> this allow as to access data quickly
