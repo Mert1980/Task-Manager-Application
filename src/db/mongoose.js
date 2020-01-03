bc97e2d7e4963472aaef937841e11e6455134155
Mert Demirok <mertdemirok80@gmail.com>
2020-01-03 23:25:55 +0100

    saving the instance to database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -17,7 +17,15 @@ const User = mongoose.model("User", {
     type: Number
   }
 });
-const me = new User({ // creating an instance of User model
-    name:'Mert',
-    age:39
-})
+const me = new User({
+  // creating an instance of User model
+  name: "Mert",
+  age: 39
+});
+me.save() // save method doesn't take arguments
+  .then(me => {
+    console.log(me);
+  })
+  .catch(error => {
+    console.log("Error:", error);
+  });
