1124b78171b00f442b897d8a2195c501c9bb52b5
Mert Demirok <mertdemirok80@gmail.com>
2020-01-05 16:33:27 +0100

    adding a new user and testing the npm validator module

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -10,11 +10,14 @@ mongoose.connect(`mongodb://127.0.0.1:27017/task-manager-api`, {
 const User = mongoose.model("User", {
   name: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   email: {
     type: String,
     required: true,
+    trim: true,
+    lowercase: true,
     validate(value) {
       if (!validator.isEmail(value)) {
         throw new Error("Email is invalid!");
@@ -23,7 +26,7 @@ const User = mongoose.model("User", {
   },
   age: {
     type: Number,
-    required: true,
+    default: 0,
     validate(value) {
       // there is not a build in validation in mongoose for numbers
       if (value < 0) {
@@ -34,9 +37,8 @@ const User = mongoose.model("User", {
 });
 
 const user = new User({
-  name: "Kaan",
-  email: "kaandemirok",
-  age: 12
+  name: "   Kaan   ",
+  email: "   KAANDEMIROK@GMAIL.com"
 });
 user
   .save()
