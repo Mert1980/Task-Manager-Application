133eac7c7e86b0e59a2faead4575d95ac2db1283
Mert Demirok <mertdemirok80@gmail.com>
2020-01-05 12:47:07 +0100

    provide validation for name field

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,6 +5,30 @@ mongoose.connect(`mongodb://127.0.0.1:27017/task-manager-api`, {
   useUnifiedTopology: true,
   useCreateIndex: true // --> this allow as to access data quickly
 });
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number
+  }
+});
+
+const user = new User({
+  name: "Kaan",
+  // age: 39
+});
+user
+  .save()
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.log(error.name, error._message);
+  });
+
 const Task = mongoose.model("Task", {
   description: {
     type: String
@@ -17,11 +41,11 @@ const myTasks = new Task({
   description: "Task Manager Application",
   completed: false
 });
-myTasks
-  .save()
-  .then(myTasks => {
-    console.log(myTasks);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// myTasks
+//   .save()
+//   .then(myTasks => {
+//     console.log(myTasks);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
