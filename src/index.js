278b3f9be0d21aad937dcfa47f9872d75be8dc4f
Mert Demirok <mertdemirok80@gmail.com>
2020-01-06 22:37:06 +0100

    adding a post request to send user info to server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 3000;
 
+app.post("/users", (req, res)=>{ // we are sending JSON data via request body
+    res.send("testing!")
+})
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
