8ab1c4abe4a771719dd49e0b379bd1970f8f6274
Mert Demirok <mertdemirok80@gmail.com>
2020-01-06 23:02:45 +0100

    setting up user model in user.js file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,45 +7,7 @@ mongoose.connect(`mongodb://127.0.0.1:27017/task-manager-api`, {
   useCreateIndex: true // --> this allow as to access data quickly
 });
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid!");
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error('Password can not contain "password"');
-      }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      // there is not a build in validation in mongoose for numbers
-      if (value < 0) {
-        throw new Error("Age must be a positive number!");
-      }
-    }
-  }
-});
+
 
 // const user = new User({
 //   name: "   Mert   ",
