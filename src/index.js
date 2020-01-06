0ad0a84c87f14c197b1fa8717f638166cf279371
Mert Demirok <mertdemirok80@gmail.com>
2020-01-06 22:52:00 +0100

    configuring express to automatically parse JSON into object so we can access it in our request handlers

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.json()); // this configures express to automatically parse JSON into
-// object so we can access it in our request handlers
+ // object so we can access it in our request handlers
 
 app.post("/users", (req, res) => {
   // we are sending JSON data via request body
