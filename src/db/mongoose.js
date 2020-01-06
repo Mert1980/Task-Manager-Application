db0c1cbbcf803cb737e4398f5a342a0d1fee4190
Mert Demirok <mertdemirok80@gmail.com>
2020-01-07 00:02:28 +0100

    task model created inside models directory, tested to add new tasks to database via postman

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,15 +6,4 @@ mongoose.connect(`mongodb://127.0.0.1:27017/task-manager-api`, {
   useCreateIndex: true // --> this allow as to access data quickly
 });
 
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-});
 
