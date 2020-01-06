8b5120f0546b4eefc8ac515d0e0c18d2ad3c66b0
Mert Demirok <mertdemirok80@gmail.com>
2020-01-06 23:05:07 +0100

    removing validator and other unnecessary example codes in mongoose.js file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,28 +1,10 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 mongoose.connect(`mongodb://127.0.0.1:27017/task-manager-api`, {
   useUnifiedTopology: true,
   useCreateIndex: true // --> this allow as to access data quickly
 });
-
-
-
-// const user = new User({
-//   name: "   Mert   ",
-//   email: "   MERTDEMIROK@GMAIL.com",
-//   password: "    Password123      ",
-//   age: 39
-// });
-// user
-//   .save()
-//   .then(result => {
-//     console.log(result);
-//   })
-//   .catch(error => {
-//     console.log(error.message);
-//   });
 
 const Task = mongoose.model("Task", {
   description: {
@@ -35,15 +17,4 @@ const Task = mongoose.model("Task", {
     default: false
   }
 });
-const myTasks = new Task({
-  description: "    ToDo Application",
-});
 
-myTasks
-  .save()
-  .then(myTasks => {
-    console.log(myTasks);
-  })
-  .catch(error => {
-    console.log(error.message);
-  });
