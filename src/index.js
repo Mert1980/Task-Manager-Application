d61ca22a44b26e915c57260a5fe3d7db488a1ab2
Mert Demirok <mertdemirok80@gmail.com>
2020-01-06 22:50:43 +0100

    configures express to automatically parse JSON into

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 3000;
 
-app.post("/users", (req, res)=>{ // we are sending JSON data via request body
-    res.send("testing!")
-})
+app.use(express.json()); // this configures express to automatically parse JSON into
+// object so we can access it in our request handlers
+
+app.post("/users", (req, res) => {
+  // we are sending JSON data via request body
+
+  res.send("testing!");
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
