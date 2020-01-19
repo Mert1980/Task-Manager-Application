fc5590343f096a9a39b0e269bc81466cdb35670c
Mert Demirok <mertdemirok80@gmail.com>
2020-01-19 09:06:25 +0100

    adding await to the example function

--- playground/5-async-await.js
+++ playground/5-async-await.js
@@ -1,10 +1,16 @@
 /* *** Async functions always return a promise. That promise is fulfilled
 with the value the developer choose to return from the function
 */
-
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(a + b);
+      }, 2000);
+    });
+  };
 const doWork = async () => {
-  throw new Error("Something went wrong");
-  return "Mert"; // --> returns Promise { 'Mert' }
+  const sum = await add(1, 99);
+  return sum;
 };
 // console.log(doWork());
 doWork()
