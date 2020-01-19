4aebe3c9a3d0028a6d1e226b530b66001fb74939
Mert Demirok <mertdemirok80@gmail.com>
2020-01-19 09:18:44 +0100

    rejecting the promise as an example

--- playground/5-async-await.js
+++ playground/5-async-await.js
@@ -4,12 +4,15 @@ with the value the developer choose to return from the function
 const add = (a, b) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      if (a < 0 || b < 0) {
+        return reject("Numbers must be non-negative");
+      }
       resolve(a + b);
     }, 2000);
   });
 };
 const doWork = async () => {
-  const sum = await add(1, 99);
+  const sum = await add(-1, 99);
   const sum2 = await add(sum, 100);
   const sum3 = await add(sum2, 100);
   return sum3;
@@ -20,5 +23,5 @@ doWork()
     console.log("Result:", result);
   })
   .catch(e => {
-    console.log("Error:", e.message);
+    console.log("Error:", e);
   });
