3837f63282ff05f8f56022760b87915e3ba75ca3
Mert Demirok <mertdemirok80@gmail.com>
2020-01-19 08:55:58 +0100

    expanding the async example with adding .then and .catch

--- playground/5-async-await.js
+++ playground/5-async-await.js
@@ -1,8 +1,15 @@
 /* *** Async functions always return a promise. That promise is fulfilled
 with the value the developer choose to return from the function
-*/ 
-const doWork = async () => {
-    return 'Mert' // --> returns Promise { 'Mert' }
-};
+*/
 
-console.log(doWork());
+const doWork = async () => {
+  return "Mert"; // --> returns Promise { 'Mert' }
+};
+// console.log(doWork());
+doWork()
+  .then(result => {
+    console.log("Result:", result);
+  })
+  .catch(e => {
+    console.log("Error:", e.message);
+  });
