b7212c9a54456e79377497fc19836fc6486aa9bc
Mert Demirok <mertdemirok80@gmail.com>
2020-01-20 19:23:13 +0100

    refactoring task routes using async await

--- src/index.js
+++ src/index.js
@@ -47,40 +47,36 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.status(404).send(task);
-    })
-    .catch(e => {
-      res.status(400).send(e.message);
-    });
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
 });
 
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then(tasks => {
-      res.send(tasks);
-    })
-    .catch(error => {
-      res.status(500).send();
-    });
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
   const _id = req.params.id;
-  Task.findById(_id)
-    .then(task => {
-      if (!task) {
-        return res.status(404).send(); // 404:not found
-      }
-      res.send(task);
-    })
-    .catch(error => {
-      res.status(500).send();
-    });
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send(); // 404:not found
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 app.listen(port, () => {
