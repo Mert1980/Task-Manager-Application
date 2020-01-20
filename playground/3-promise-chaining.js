39a7f184f3d16aed0950df252c441315a9313b1c
Mert Demirok <mertdemirok80@gmail.com>
2020-01-20 15:33:02 +0100

    update age and count in User model with async await function

--- playground/3-promise-chaining.js
+++ playground/3-promise-chaining.js
@@ -1,13 +1,27 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
 
-User.findByIdAndUpdate("5e15fe61e1d4501ea8d015c2", { age: 12 })
-  .then(result => {
-    console.log(result);
-    return User.countDocuments({ age: 12 });
-  })
-  .then(result => {
-    console.log(result);
+// User.findByIdAndUpdate("5e15fe61e1d4501ea8d015c2", { age: 12 })
+//   .then(result => {
+//     console.log(result);
+//     return User.countDocuments({ age: 12 });
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(e => {
+//     console.log(e.message);
+//   });
+
+const updateAgeandCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeandCount("5e13b1d0f36c40273d39cc8b", 12)
+  .then(count => {
+    console.log(count);
   })
   .catch(e => {
     console.log(e.message);
