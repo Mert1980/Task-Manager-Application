d879bd697c37a42bc23a3dd998ffa552c9245ce0
Mert Demirok <mertdemirok80@gmail.com>
2020-01-25 17:26:55 +0100

    creating new userSchema

--- src/models/user.js
+++ src/models/user.js
@@ -1,44 +1,46 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const User = mongoose.model("User", {
-    name: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      lowercase: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error("Email is invalid!");
-        }
-      }
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 7,
-      validate(value) {
-        if (value.toLowerCase().includes("password")) {
-          throw new Error('Password can not contain "password"');
-        }
-      }
-    },
-    age: {
-      type: Number,
-      default: 0,
-      validate(value) {
-        // there is not a build in validation in mongoose for numbers
-        if (value < 0) {
-          throw new Error("Age must be a positive number!");
-        }
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid!");
       }
     }
-  });
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password can not contain "password"');
+      }
+    }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      // there is not a build in validation in mongoose for numbers
+      if (value < 0) {
+        throw new Error("Age must be a positive number!");
+      }
+    }
+  }
+})
 
-  module.exports = User
+const User = mongoose.model("User", userSchema);
+
+module.exports = User
