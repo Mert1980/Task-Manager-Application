c24321d6f8538b580f9922ece81ce7723dc6824a
Mert Demirok <mertdemirok80@gmail.com>
2020-01-26 21:16:25 +0100

    sending the user back if found or throw an error

--- src/routers/user.js
+++ src/routers/user.js
@@ -16,9 +16,13 @@ router.post("/users", async (req, res) => {
 
 router.post("/users/login", async (req, res) => {
   try {
-    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
   } catch (e) {
-
+    res.status(400).send();
   }
 });
 
