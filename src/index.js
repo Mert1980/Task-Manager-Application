02da497442e04c47d587fa9e3e9eb9d39612c8c5
Mert Demirok <mertdemirok80@gmail.com>
2020-01-26 21:50:41 +0100

    wipe the content of myFunction in indej.js to define JSON web tokens

--- src/index.js
+++ src/index.js
@@ -16,19 +16,9 @@ app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
 
-const bcrypt = require("bcryptjs");
+
 
 const myFunction = async () => {
-  const password = "Red12345"
-  const hashedPassword = await bcrypt.hash(password, 8) 
-  // 8 is the number of rounds that the hashing algorithm is to be executed.
-  // 8 is the balance between speed and security
-  console.log(password);
-  console.log(hashedPassword);
-
-  const isMatch = await bcrypt.compare("Red12345", hashedPassword);
-  console.log(isMatch);
+  
 };
 myFunction();
-// Encryption algorithms are reverseble: mert --> dsldfjsldfhsneouwen --> mert 
-// Hashing algorithms are not reverseble: mert --> ekfjsdlfjp43p4jfldcjdcn@434
