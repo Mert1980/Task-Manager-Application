8f0f2cf7b4393467c9581aaa18333c01b51a1b6a
Mert Demirok <mertdemirok80@gmail.com>
2020-01-28 22:19:20 +0100

    Add newly generated token to tokens array making sure that token shows up in the database

--- src/models/user.js
+++ src/models/user.js
@@ -56,6 +56,8 @@ userSchema.methods.generateAuthToken = async function() {
   const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
   // {} --> payload, "" --> our secret
   // convert object ID to string
+  user.tokens = user.tokens.concat({token});
+  await user.save();
   return token;
 };
 
