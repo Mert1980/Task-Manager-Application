53c64bf16010d9e993251fec6e3c31f698095a46
Mert Demirok <mertdemirok80@gmail.com>
2020-01-31 20:01:19 +0100

    add a property onto request to store user so route handlers will be able to access it later on

--- src/middelware/authentication.js
+++ src/middelware/authentication.js
@@ -5,12 +5,18 @@ const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace('Bearer ', '');
     const decoded = jwt.verify(token, 'thisismynewcourse');
-    const user = await User.findOne({id: decoded._id, "tokens.token": token });
+    const user = await User.findOne({id: decoded._id, 'tokens.token': token });
     // find a user with a correct ID who has authentication token still stored. 
     // If the user logs out that means this token is still valid
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.user = user;
+    next();
   } catch (e) {
     res.status(401).send({ error: "Please authenticate!" });
   }
 };
-
 module.exports = auth;
