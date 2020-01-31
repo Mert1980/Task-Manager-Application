8ff01b46bc3d7b3856aa0a2ba276c60b3a681e1a
Mert Demirok <mertdemirok80@gmail.com>
2020-01-31 14:29:39 +0100

    receiving the auth jwt token via request header

--- src/middelware/authentication.js
+++ src/middelware/authentication.js
@@ -2,8 +2,12 @@ const jwt = require("jsonwebtoken"); // to validate the token being provided
 const User = require("../models/user"); // to find token in database
 
 const auth = async (req, res, next) => {
-  
-
+  try {
+    const token = req.header("Authorization");
+    console.log(token);
+  } catch (e) {
+    res.status(401).send({ error: "Please authenticate!" });
+  }
 };
 
 module.exports = auth;
