da13afb3982da615dfcb510ab98d89c57871efb9
Mert Demirok <mertdemirok80@gmail.com>
2020-02-27 21:52:00 +0100

    making user that only authenticated user can delete his/her tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -41,7 +41,7 @@ router.get("/tasks/:id", auth, async (req, res) => {
     }
   });
   
-router.patch("/tasks/:id", async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ["completed", "description"];
     const isValidOperation = updates.every(update =>
@@ -53,22 +53,24 @@ router.patch("/tasks/:id", async (req, res) => {
     }
   
     try {
-      const task = await Task.findById(req.params.id);
+      // const task = await Task.findById(req.params.id);
+      const task = await Task.findOne({_id:req.params.id, owner:req.user._id}) 
 
-      updates.forEach((update)=> task[update] = req.body[update]);
-      await task.save()
-      
       if (!task) {
         res.status(404).send();
       }
+
+      updates.forEach((update)=> task[update] = req.body[update]);
+      await task.save()
+           
       res.send(task);
     } catch (e) {
       res.status(400).send(e.message);
     }
   });
   
-router.delete("/tasks/:id", async (req, res) => {
-    const task = await Task.findByIdAndDelete(req.params.id);
+router.delete("/tasks/:id", auth, async (req, res) => {
+    const task = await Task.findOneAndDelete({_id:req.params.id, owner:req.user._id});
     try {
       if (!task) {
         return res.status(404).send();
