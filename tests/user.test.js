6764961fc85e9a8eb8ef0efd0e07730b375da05a
Mert Demirok <mertdemirok80@gmail.com>
2020-04-25 19:48:26 +0200

    create a test case for signup user endpoint

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,2 +1,11 @@
-const request = require('supertest')
-const app = require('../src/app')
+const request = require("supertest");
+const app = require("../src/app");
+
+// Test signup router
+test("Should signup a user", async () => {
+  await request(app).post("/users").send({
+    name: "Mert",
+    email: "mertdemirok80@gmail.com",
+    password: "Green12345",
+  }).expect(201)
+});
