7a11f7ae5ce65e4d3defffde93fcf696a1ceb529
Mert Demirok <mertdemirok80@gmail.com>
2020-04-25 20:22:01 +0200

    Create a new user before testing the signup endpoint

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,11 +2,20 @@ const request = require("supertest");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
+const userOne = {
+    name: "Yigit",
+    email: "example@gmail.com",
+    password: "Green12345"
+}
+
 // This function runs before each test case in this test suite
 // Delete all the users before creating a new user in database
 beforeEach(async () => {
   await User.deleteMany();
+  await new User(userOne).save()
 });
+
+
 
 // Test signup router
 // This test case fails 2nd time (due to duplication of mail address )
