d40dcf80e1a7a37025bcea33b9f010f076f36331
Mert Demirok <mertdemirok80@gmail.com>
2020-04-25 20:11:09 +0200

    beforeEach function runs before each test case in this test suite

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,13 +1,23 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require('../src/models/user')
+
+// This function runs before each test case in this test suite
+beforeEach(() => {
+  console.log("beforeEach");
+});
+
 
 // Test signup router
 // This test case fails 2nd time (due to duplication of mail address )
 // unless jest lifecycle methods are defined in advance
 test("Should signup a user", async () => {
-  await request(app).post("/users").send({
-    name: "Mert",
-    email: "mertdemirok80@gmail.com",
-    password: "Green12345",
-  }).expect(201)
+  await request(app)
+    .post("/users")
+    .send({
+      name: "Mert",
+      email: "mertdemirok80@gmail.com",
+      password: "Green12345",
+    })
+    .expect(201);
 });
