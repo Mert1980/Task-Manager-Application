5bd5ac71d6d0eef7b6b9c9cf91b737dbecfbc171
Mert Demirok <mertdemirok80@gmail.com>
2020-04-25 20:04:56 +0200

    comment the test case for signup user endpoint

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,6 +2,8 @@ const request = require("supertest");
 const app = require("../src/app");
 
 // Test signup router
+// This test case fails 2nd time (due to duplication of mail address )
+// unless jest lifecycle methods are defined in advance
 test("Should signup a user", async () => {
   await request(app).post("/users").send({
     name: "Mert",
