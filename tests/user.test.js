908ce3c63ff3ad05176012e4bf11dc22d0380afb
Mert Demirok <mertdemirok80@gmail.com>
2020-04-25 20:31:55 +0200

    test user login

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,21 +2,19 @@ const request = require("supertest");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
-// Create a new user 
+// Create a new user to test other test cases (login, update etc)
 const userOne = {
-    name: "Yigit",
-    email: "example@gmail.com",
-    password: "Green12345"
-}
+  name: "Yigit",
+  email: "example@gmail.com",
+  password: "Green12345",
+};
 
 // This function runs before each test case in this test suite
 // Delete all the users before creating a new user in database
 beforeEach(async () => {
   await User.deleteMany();
-  await new User(userOne).save()
+  await new User(userOne).save();
 });
-
-
 
 // Test signup router
 // This test case fails 2nd time (due to duplication of mail address )
@@ -30,4 +28,11 @@ test("Should signup a user", async () => {
       password: "Green12345",
     })
     .expect(201);
+});
+
+test("Should login existing user", async () => {
+  await request(app).post("/users/login").send({
+      email: userOne.email,
+      password : userOne.password
+  }).expect(200)
 });
