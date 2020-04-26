870081b15bef7526eda5aba5fe25abbec562742b
Mert Demirok <mertdemirok80@gmail.com>
2020-04-27 01:53:07 +0200

    Create a user ID and token to test read profile and delete account

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,22 @@
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
+
+// Create a user ID to test read profile and delete account
+const userOneId = new mongoose.Types.ObjectId()
+
 // Create a new user to test other test cases (login, update etc)
 const userOne = {
+  _id : userOneId,
   name: "Yigit",
   email: "example@gmail.com",
   password: "Green12345",
+  tokens: [{
+    token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+  }]
 };
 
 // This function runs before each test case in this test suite
