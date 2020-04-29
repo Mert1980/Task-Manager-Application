abef1cbd9ce671d5a52bf98933133ef5fe428f0a
Mert Demirok <mertdemirok80@gmail.com>
2020-04-29 06:45:39 +0200

    test creating a task

--- tests/task.test.js
+++ tests/task.test.js
@@ -7,6 +7,17 @@ const {userOneId, userOne, setupDatabase} = require('../tests/fixtures/db')
 // Delete all the users before creating a new user in database
 beforeEach(setupDatabase);
 
-test('Should create task user', () => {
+// test create task
+test('Should create task user', async () => {
+    const response = await request(app)
+    .post("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+        description: "This is for testing"
+    })
+    .expect(201)
+    const task = await Task.findById(response.body._id)
+    expect(task).not.toBeNull
+    expect(task.completed).toEqual(false)
 
 })
